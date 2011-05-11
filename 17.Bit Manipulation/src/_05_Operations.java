ef4ef65de83468c18d83bd0f7f8c404cd8d2d8d8
Mayank <patelmayank232007@gmail.com>
2011-05-11 11:38:25 +0530

    operations over bit level

---
+++ 17.Bit Manipulation/src/_05_Operations.java
@@ -0,0 +1,26 @@
+public class _05_Operations {
+    public static int getI(int n,int i){
+        int bitmask = 1<<i;
+        if((n&bitmask) == 0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+
+    public static int setI(int n,int i){
+        int bitmask = 1<<i;
+        return n|bitmask;
+    }
+
+    public static int clearI(int n,int i){
+        int bitmask = ~(1<<i);
+        return n&bitmask;
+    }
+    public static void main(String[] args) {
+        System.out.println(getI(8,0));
+        System.out.println(setI(10,2));
+        System.out.println(clearI(10,1));
+    }
+}
