d441b64f0e1bece248e6b0f91ff9859bd95ca4a6
mayank <patelmayank232007@gmail.com>
2025-01-09 11:18:36 +0530

    basics of java program

---
+++ 06.Variable/src/datatypes_in_java.java
@@ -0,0 +1,14 @@
+public class datatypes_in_java {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        float price = 10.5f;
+        int number = 25;
+        short n = 50;
+        System.out.println(var);
+
+    }
+}
