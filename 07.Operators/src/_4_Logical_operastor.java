e12982465b85f31bce2e931ffb43752f792e8342
mayank <patelmayank232007@gmail.com>
2025-01-22 01:49:24 +0530

    operators

--- 07.Operators/src/_4_Logical_operastor.java
+++ 07.Operators/src/_4_Logical_operastor.java
@@ -1,5 +1,7 @@
 public class _4_Logical_operastor {
     public static void main(String[] args) {
-        System.out.println("mayank is a good boy");
+        System.out.println((3>2)||(3>5));
+        System.out.println((3>5)&&(3>1));
+        System.out.println(!(3>2));
     }
 }
