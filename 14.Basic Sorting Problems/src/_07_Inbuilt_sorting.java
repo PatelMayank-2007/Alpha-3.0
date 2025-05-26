7cfb3edb29be8453bea10e631efe99e912e5b63d
Mayank <patelmayank232007@gmail.com>
2025-05-27 01:30:48 +0530

    inbuilt sorting

---
+++ 14.Basic Sorting Problems/src/_07_Inbuilt_sorting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class _07_Inbuilt_sorting {
+    public static void main(String[] args) {
+        int arry[] = {34,65,12,89,23};
+        Arrays.sort(arry);
+        for(int i = 0 ;i<=arry.length ;i++){
+            System.out.print(arry[i] + " ");
+        }
+    }
+}
