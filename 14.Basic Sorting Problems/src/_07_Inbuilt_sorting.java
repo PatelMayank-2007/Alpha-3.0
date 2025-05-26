4bc047984692a0b097811cf55dd7bf0e0b5b2b90
Mayank <patelmayank232007@gmail.com>
2025-05-27 01:38:52 +0530

    inbuilt sorting

--- 14.Basic Sorting Problems/src/_07_Inbuilt_sorting.java
+++ 14.Basic Sorting Problems/src/_07_Inbuilt_sorting.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class _07_Inbuilt_sorting {
     public static void main(String[] args) {
         int arry[] = {34,65,12,89,23};
-        Arrays.sort(arry);
-        for(int i = 0 ;i<=arry.length ;i++){
-            System.out.print(arry[i] + " ");
+        //Arrays.sort(arry);   This is for whole array sorting
+        Arrays.sort(arry,0,3); //this is for sorting of array in yhe range we want but last element in non-inclusive so for 2 index have to pass 3.
+        for(int i = 1 ;i<=arry.length ;i++){
+            System.out.print(arry[i-1] + " ");
         }
     }
 }
