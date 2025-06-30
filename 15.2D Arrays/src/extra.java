8c0a9bf9ded0856fd5cecf90b2f1000a6ffefb3a
Mayank <patelmayank232007@gmail.com>
2025-07-01 03:21:15 +0530

    maximum form 2D array

---
+++ 15.2D Arrays/src/extra.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class extra {
+    public void printNumber(Scanner sc) {
+//        Scanner sc = new Scanner(System.in);
+        int x;
+        x=sc.nextInt();
+        System.out.print(x);
+
+    }
+}
