fc5a88523f2558e91303365f733dbf6fb7cac60b
Mayank <patelmayank232007@gmail.com>
2025-07-05 21:08:24 +0530

    use of CharAt method to print string

---
+++ 16.Strings/src/_01_CharAt_method.java
@@ -0,0 +1,18 @@
+public class _01_CharAt_method {
+    public static int printStr(String str){
+        for(int i=0;i<=str.length();i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String firstname = "Janvi";
+        String lastname = "Patel";
+        String fullname = firstname + " " + lastname;
+
+        printStr(fullname);
+
+    }
+}
