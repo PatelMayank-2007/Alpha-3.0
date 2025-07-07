c46f3921cd7a7937c29956763c0e60b350be7220
Mayank <patelmayank232007@gmail.com>
2025-07-07 23:53:58 +0530

    String builder

---
+++ 16.Strings/src/__04_String_Builedr.java
@@ -0,0 +1,9 @@
+public class __04_String_Builedr {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a'; ch<='z';ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
