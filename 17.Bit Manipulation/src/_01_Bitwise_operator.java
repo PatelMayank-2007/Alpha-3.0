5dca63777c4c7d7c1c0fc42131e054f631418eff
Mayank <patelmayank232007@gmail.com>
2025-07-12 19:43:12 +0530

    Bitwise Operator

---
+++ 17.Bit Manipulation/src/_01_Bitwise_operator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class _01_Bitwise_operator {
+    public static void main(String[] args) {
+        System.out.println(5&6);
+        System.out.println(5|6);
+        System.out.println(5^6);
+        System.out.println(~5);
+        System.out.println(~0);
+        System.out.println(5<<2);
+        System.out.println(7>>1);
+    }
+}
