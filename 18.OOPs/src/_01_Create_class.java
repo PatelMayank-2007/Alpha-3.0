5b02241a3aa789d6a47710e51e13774900135dfa
Mayank <patelmayank232007@gmail.com>
2025-07-18 00:11:29 +0530

    class creates

---
+++ 18.OOPs/src/_01_Create_class.java
@@ -0,0 +1,23 @@
+public class _01_Create_class {
+
+        public static void main(String args[]){
+            //creating a pen object called p1
+            Pen p1 = new Pen();
+            p1.setColor("Blue");
+            p1.size(5);
+            System.out.println(p1.color);
+            System.out.println(p1.tip);
+        }
+    }
+    class Pen{
+        String color;
+        int tip;
+        void setColor(String newColor){
+            color = newColor;
+        }
+        void size(int newTip){
+            tip = newTip;
+        }
+    }
+
+
