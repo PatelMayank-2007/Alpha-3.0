6626cee2030e0ffa536e4c1dd121a212ae5512a8
Mayank <patelmayank232007@gmail.com>
2025-07-25 03:53:18 +0530

    inserting the new node

--- 24.Linked List (Part-1)/src/_01_Linked_list.java
+++ 24.Linked List (Part-1)/src/_01_Linked_list.java
@@ -55,16 +55,35 @@ public class _01_Linked_list {
         System.out.println("null");
     }
 
+
+    public void add(int index, int data) {
+        // Step 1: Create a new node with the provided data.
+        Node newNode = new Node(data);
+        // Step 2: Create a temporary pointer to traverse the linked list, starting from head.
+        Node temp = head;
+        // Step 3: Initialize a counter i to track the current position.
+        int i = 0;
+        // Step 4: Move temp to the node just before the desired index (index - 1).
+        while (i < index - 1) {
+            temp = temp.next; // move to the next node
+            i++;
+        }
+        // Now, temp is at (index - 1), and we are ready to insert the new node.
+        // Step 5: temp node next is pointing towards next position in linked list now we have to insert in between this two so temp's next value we will store in newNode's next.
+        newNode.next = temp.next;
+        // Step 6: Point the previous node's (at index - 1) next to the new node, linking it into the list.
+        temp.next = newNode;
+    }
+
+
     public static void main(String[] args) {
         _01_Linked_list ll = new _01_Linked_list();
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addlast(3);
-        ll.print();
         ll.addlast(4);
+        ll.print();
+        ll.add(2,9);
         ll.print();
     }
 }
