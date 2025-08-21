81563316e88b97e913f71b196cd40725a0bf4422
Mayank <patelmayank232007@gmail.com>
2025-08-21 23:53:29 +0530

    SPIRAL matrix

--- 15.2D Arrays/src/_04_Spiral_matrix.java
+++ 15.2D Arrays/src/_04_Spiral_matrix.java
@@ -37,9 +37,9 @@ public class _04_Spiral_matrix {
 
     public static void main(String[] args) {
         int matrix[][] = {{1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}};
+                          {5, 6, 7, 8},
+                          {9, 10, 11, 12},
+                          {13, 14, 15, 16}};
         print_spiral(matrix);
 
     }
